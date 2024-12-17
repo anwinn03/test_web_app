1ca848609e706851a8616deb2cee485531ddf335
anwinn03 <atn24@georgetown.edu>
2024-12-16 23:53:22 -0500

    update

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
-from routes import configure_routes
+from routes.stock_routes import configure_routes
 
 app = Flask(__name__)
 
-# Register routes
+# Register routes from routes folder
 configure_routes(app)
 
 if __name__ == "__main__":
